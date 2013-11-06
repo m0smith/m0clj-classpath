dd49085d9a46bc4ed2fd30c3c0f1624d0c7252cc
Matthew O. Smith <matt@m0smith.com>
2013-11-06 15:58:31 -0700

    Increased version for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject m0clj-classpath "0.1.0"
+(defproject m0clj-classpath "0.1.1"
   :description "Interact with java classpath from Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
