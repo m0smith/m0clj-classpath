69fe5e8ae59f2495c44d485d90c5fdf45808c693
Matthew O. Smith <matt@m0smith.com>
2013-11-29 09:37:52 -0700

    Test

---
+++ test/m0clj_classpath/W.java
@@ -0,0 +1,9 @@
+package m0clj_classpath;
+
+public class W {
+    public static void main(String []args) {
+	m0clj_classpath.tools w = new m0clj_classpath.tools();
+	System.out.println(w.which("java.lang.Object"));
+    }
+
+}
